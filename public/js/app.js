557c0316d58e94b0285673b18e58da9b930fe662
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-12-14 17:04:56 +0530

    feat: adding  validations

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 //geting html elements to manipulate
+// const inputBoxEls = document.querySelectorAll("input");
+// console.log(inputBoxEls);
 const maleInputEl = document.getElementById("male-name");
 const femaleInputEl = document.getElementById("female-name");
 const predictBtn = document.getElementById("predict-btn");
@@ -43,54 +45,60 @@ const flamesObject = {
 
 function predictRelationship() {
   //get input names from input box and transform to lower case
-  let maleName = maleInputEl.value.toLowerCase();
-  let femaleName = femaleInputEl.value.toLowerCase();
 
   // console.log(maleName);
   // console.log(femaleName);
 
-  //replace all spaces from names and transforms string to array
-  let maleArray = maleName.replaceAll(" ", "").split("");
-  let femaleArray = femaleName.replaceAll(" ", "").split("");
+  //validatte function return validate value
+  let getInputNames = validation();
+  let [maleName, femaleName] = getInputNames;
 
-  // console.log(maleArray);
-  // console.log(femaleArray);
+  // predict error from undifiend variable using if
+  if (maleName !== undefined && femaleName !== undefined) {
+    //replace all spaces from names and transforms string to array
+    let maleArray = maleName.toLowerCase().replaceAll(" ", "").split("");
+    let femaleArray = femaleName.toLowerCase().replaceAll(" ", "").split("");
 
-  let relationshipIndex = 0;
-  //passing arrays to getrelationship function based on the length of the arrays
-  if (maleArray.length > femaleArray.length) {
-    relationshipIndex = getRelationship(maleArray, femaleArray);
-  } else {
-    relationshipIndex = getRelationship(femaleArray, maleArray);
+    // console.log(maleArray);
+    // console.log(femaleArray);
+
+    let relationshipIndex = 0;
+    //passing arrays to getrelationship function based on the length of the arrays
+    if (maleArray.length > femaleArray.length) {
+      relationshipIndex = getRelationship(maleArray, femaleArray);
+    } else {
+      relationshipIndex = getRelationship(femaleArray, maleArray);
+    }
+
+    //setting flames object result and result img to result page
+    let relationship = flamesObject[relationshipIndex];
+    // console.log(relationship);
+    resultEl.innerText = relationship.result;
+    resultImgEl.src = relationship.resultImg;
+
+    //to show loader remove hidden from classlist
+    loaderEl.classList.remove("hidden");
+
+    //time out function to hide loader after 4.2 seconds
+    setTimeout(() => {
+      loaderEl.classList.add("hidden");
+    }, 4200);
+
+    // remove hidden to show result page
+    resultPageEl.classList.remove("hidden");
+
+    //emptying input box after get result
+    maleInputEl.value = "";
+    femaleInputEl.value = "";
+
+    //add event listner to close result page
+    predictMoreBtn.addEventListener("click", () => {
+      resultPageEl.classList.add("hidden");
+    });
   }
-
-  //setting flames object result and result img to result page
-  let relationship = flamesObject[relationshipIndex];
-  console.log(relationship);
-  resultEl.innerText = relationship.result;
-  resultImgEl.src = relationship.resultImg;
-
-  //to show loader remove hidden from classlist
-  loaderEl.classList.remove("hidden");
-
-  //time out function to hide loader after 4.2 seconds
-  setTimeout(() => {
-    loaderEl.classList.add("hidden");
-  }, 4200);
-
-  // remove hidden to show result page
-  resultPageEl.classList.remove("hidden");
-
-  //emptying input box after get result
-  maleInputEl.value = "";
-  femaleInputEl.value = "";
-
-  //add event listner to close result page
-  predictMoreBtn.addEventListener("click", () => {
-    resultPageEl.classList.add("hidden");
-  });
 }
 
+//core function to strike out same value in two names
 function getRelationship(array1, array2) {
   for (let index = 0; index < array1.length; index++) {
     let foundindex = array2.indexOf(array1[index]);
@@ -105,19 +113,37 @@ function getRelationship(array1, array2) {
   console.log(array2);
 
   let combinedArray = [...array1, ...array2];
-  console.log(combinedArray);
+  // console.log(combinedArray);
   let resultArray = [];
   combinedArray.forEach((e) => {
     if (e !== 0) {
       resultArray.push(e);
     }
   });
-  console.log(resultArray);
+  // console.log(resultArray);
 
   let uniqueValue = resultArray.length;
   let finalVal = uniqueValue % 6; // [f,l,a,m,e,s] => length = 6;
 
   return finalVal;
+}
+
+function validation() {
+  // merging two input box el to validate
+  let inputFieldArray = [maleInputEl, femaleInputEl];
+  let inputNames = [];
+  inputFieldArray.forEach((el) => {
+    if (el.value !== "") {
+      inputNames.push(el.value);
+    } else {
+      el.nextElementSibling.innerText = "required";
+    }
+    el.addEventListener("keydown", (e) => {
+      el.nextElementSibling.innerText = "";
+    });
+  });
+
+  return inputNames;
 }
 
 predictBtn.addEventListener("click", predictRelationship);
