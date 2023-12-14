3afd8d9552c1e3b0849fc88fe9495989815a23bd
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-12-14 22:17:29 +0530

    finalcheck

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./assets/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./assets/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
